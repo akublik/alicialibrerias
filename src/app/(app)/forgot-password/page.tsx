0b70ddc4f8f76fadae54fe092527317e94958711
André Kublik <info@estudionet.net>
2025-06-21 16:53:38 +0000

    sigue igual

--- src/app/(app)/forgot-password/page.tsx
+++
@@ -1,3 +0,0 @@
-// The content of this file has been removed to resolve a routing conflict with
-// /src/app/(auth)/forgot-password/page.tsx.
-// By not exporting a default component, this file no longer creates a page route.
