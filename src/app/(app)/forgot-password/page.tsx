3caedb02090db965b2ef5cdc13c0735ec629bd68
André Kublik <info@estudionet.net>
2025-06-21 16:29:07 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/app/(app)/forgot-password/page.tsx
+++ src/app/(app)/forgot-password/page.tsx
@@ -1,4 +1,3 @@
-// src/app/(app)/forgot-password/page.tsx
 // The content of this file has been removed to resolve a routing conflict with
 // /src/app/(auth)/forgot-password/page.tsx.
 // By not exporting a default component, this file no longer creates a page route.
