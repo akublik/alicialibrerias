539c89af3956769f25f8caf741f5e2ff56217c57
André Kublik <info@estudionet.net>
2025-06-22 18:33:44 +0000

    Error: Invalid src prop (https://alicialectura.com/wp-content/uploads/20

--- next.config.ts
+++ next.config.ts
@@ -16,6 +16,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'alicialectura.com',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
 };
