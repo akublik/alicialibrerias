f436f0e897393892072a11337e8c7f85a9817134
André Kublik <info@estudionet.net>
2025-06-23 15:57:41 +0000

    veo un doble menu ahora en el top

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -1,16 +1,7 @@
-import { Navbar } from '@/components/layout/Navbar';
-import { Footer } from '@/components/layout/Footer';
-
 export default function AppLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  return (
-    <div className="flex min-h-screen flex-col">
-      <Navbar />
-      <main className="flex-grow">{children}</main>
-      <Footer />
-    </div>
-  );
+  return <>{children}</>;
 }
