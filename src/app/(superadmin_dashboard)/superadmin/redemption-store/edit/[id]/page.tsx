7551c0ae82d48a2cbe87058c39143e3fb8253876
André Kublik <info@estudionet.net>
2025-06-28 22:48:18 +0000

    ReferenceError: FormDescription is not defined     at Object.render (web

--- src/app/(superadmin_dashboard)/superadmin/redemption-store/edit/[id]/page.tsx
+++ src/app/(superadmin_dashboard)/superadmin/redemption-store/edit/[id]/page.tsx
@@ -5,7 +5,15 @@ import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
 import * as z from "zod";
 import { Button } from "@/components/ui/button";
-import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 import { Textarea } from "@/components/ui/textarea";
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
