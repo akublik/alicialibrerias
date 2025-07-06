bc9fad8a538733f900a9e8cf189f91c05a0138c9
André Kublik <info@estudionet.net>
2025-07-06 18:45:42 +0000

    me da este error: Unhandled Runtime Error Error: Invalid src prop (https

---
+++ src/next.config.js
@@ -0,0 +1,40 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'alicialectura.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'firebasestorage.googleapis.com',
+        port: '',
+        pathname: '/v0/b/alicia-lee.firebasestorage.app/o/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'ejemplo.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'http',
+        hostname: 'canal.bibliomanager.com',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
+};
+
+module.exports = nextConfig;
